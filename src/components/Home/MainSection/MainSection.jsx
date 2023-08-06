78c0546b0d71e1be5313dd64ce27fa2cc778c3e8
hitesh <kumarhk1002@gmail.com>
2023-08-06 11:03:44 +0530

    loading skeletain fix

--- src/components/Home/MainSection/MainSection.jsx
+++ src/components/Home/MainSection/MainSection.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./MainSection.css";
 import axios from "axios";
 import AllJobs from "../AllJobsSection/AllJobs";
-import LoadingSkeletain from "./loadingSkeletain";
+import LoadingSkeletain from "./LoadingSkeletain";
 
 const MainSection = () => {
   const [jobs, setJobs] = useState([]);
